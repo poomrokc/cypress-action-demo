a3593de856974fe9396c6d74c721b01c2433d10c
Poompatai Puntitpong <poomtapai@hotmail.com>
2021-06-02 15:55:27 +0700

    Cypress Commit

--- cypress/integration/cy1.spec.js
+++ cypress/integration/cy1.spec.js
@@ -1,7 +1,7 @@
 describe('HelloWorld', () => {
     it("should show", function () {
         cy.visit("/");
-        cy.wait(10000);
+        cy.wait(100000);
         cy.get('h1').contains('Welcome to Your Vue.js App')
     });
 })
