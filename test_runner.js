d6932f7b5c808d1d92708aed21b1a5a364a756c0
Poompatai Puntitpong <poomtapai@hotmail.com>
2021-06-10 16:40:14 +0700

    Remove debug

--- test_runner.js
+++ test_runner.js
@@ -84,10 +84,8 @@ const github_action_start = async(token, test_branch, test_run_id, build_file_ur
             'Authorization': `token ${token}`,
             'Accept': 'application/vnd.github.v3+json'
         }});
-        console.log(res);
         return true;
     } catch(err) {
-        console.log(err);
         return false;
     }
 }
